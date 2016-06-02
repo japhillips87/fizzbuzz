f490bc4ca706a54efe5f824c77fb5849d528ceda
Josh Phillips <japhillips87@gmail.com>
2016-06-02 14:48:04 -0400

    add number and value fields to fizzbuzz/index page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'fizzbuzz#index'
+  root 'fizzbuzzs#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
