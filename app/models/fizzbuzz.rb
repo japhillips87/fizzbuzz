6995729620ff3e9966e058a1a4b3005e3b241f0e
Josh Phillips <japhillips87@gmail.com>
2016-06-02 14:47:27 -0400

    add #value method to fizzbuzz

--- app/models/fizzbuzz.rb
+++ app/models/fizzbuzz.rb
@@ -1,4 +1,11 @@
 class Fizzbuzz < ActiveRecord::Base
 
   validates_uniqueness_of :number
+
+  def value
+    computed_value = ''
+    computed_value += 'Fizz' if number % 3 == 0
+    computed_value += 'Buzz' if number % 5 == 0
+    computed_value
+  end
 end
--- spec/models/fizzbuzz_spec.rb
+++ spec/models/fizzbuzz_spec.rb
@@ -8,4 +8,26 @@ RSpec.describe Fizzbuzz, type: :model do
       expect(invalid).not_to be_valid
     end
   end
+
+  describe '#value' do
+    it 'returns fizz when number is a multiple of 3' do
+      fizzbuzz = Fizzbuzz.new number: 3
+      expect(fizzbuzz.value).to eq 'Fizz'
+    end
+
+    it 'returns buzz when number is a multiple of 5' do
+      fizzbuzz = Fizzbuzz.new number: 5
+      expect(fizzbuzz.value).to eq 'Buzz'
+    end
+
+    it 'returns nothing when a number is not a multiple of 3 or 5' do
+      fizzbuzz = Fizzbuzz.new number: 4
+      expect(fizzbuzz.value).to eq ''
+    end
+
+    it 'returns FizzBuzz when a number is a multiple of 3 and 5' do
+      fizzbuzz = Fizzbuzz.new number: 15
+      expect(fizzbuzz.value).to eq 'FizzBuzz'
+    end
+  end
 end
